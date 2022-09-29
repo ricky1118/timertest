27e0def094b35bc35af6ae89df8dd43e761fd265
RickyYu <ricky1118@foxmail.com>
2022-09-29 18:06:30 +0800

    修正了LOGI打印报错问题

--- main/timertest.c
+++ main/timertest.c
@@ -30,7 +30,7 @@ esp_timer_handle_t test_o_handle = 0 ;
 esp_timer_create_args_t test_once_arg = {
                                             .callback = &test_timer_once_cb,//设置回调函数
                                             .arg = NULL,//不带参数
-                                            .name ="TestOnceTimer"
+                                            .name ="TestOnceTimer"//
                                             };
 //定义一个周期重复运行的定时器结构体
 esp_timer_create_args_t test_period_arg ={
@@ -41,14 +41,14 @@ esp_timer_create_args_t test_period_arg ={
 
 void test_timer_periodic_cb(void *arg){
         int64_t tick = esp_timer_get_time();
-//        ESP_LOGI(TAG,"方法回调名字：%s,距离定时器开启时间间隔 =%lld \r\n",test_period_arg.name,tick);
+        ESP_LOGI(TAG,"方法回调名字：%s,距离定时器开启时间间隔 =%lld \r\n",test_period_arg.name,tick);
 
         if(tick >100000000){
             //停止定时器工作，并获取是否停止成功
             esp_err_t err = esp_timer_stop(test_p_handle);
- //           ESP_LOGI(TAG,"要停止的定时器名称：%s,是否停止成功：%s",test_period_arg.name,err = ESP_OK ? "OK!\r\n" : "failed!\r\n");
+            ESP_LOGI(TAG,"要停止的定时器名称：%s,是否停止成功：%s",test_period_arg.name,err == ESP_OK ? "OK!\r\n" : "failed!\r\n");
             err = esp_timer_delete(test_p_handle);
-//            ESP_LOGI(TAG,"要删除定时器名字：%s,是否停止成功：%s",test_period_arg.name,err =ESP_OK ? "OK!\r\n" : "failed!\r\n");
+            ESP_LOGI(TAG,"要删除定时器名字：%s,是否删除成功：%s",test_period_arg.name,err ==ESP_OK ? "OK!\r\n" : "failed!\r\n");
 
         }
          // 低电平
@@ -62,11 +62,10 @@ void test_timer_once_cb(void *arg) {
 
 	int64_t tick = esp_timer_get_time();
 
-//	ESP_LOGI(TAG,"方法回调名字: %s , 距离定时器开启时间间隔 = %lld \r\n", __func__, tick);
+	ESP_LOGI(TAG,"方法回调名字: %s , 距离定时器开启时间间隔 = %lld \r\n", test_once_arg.name, tick);
 
 	esp_err_t err = esp_timer_delete(test_o_handle);
-
-//	ESP_LOGI(TAG,"要删除的定时器名字：%s , 是否停止成功：%s", test_once_arg.name,err == ESP_OK ? "ok!\r\n" : "failed!\r\n");
+	ESP_LOGI(TAG,"要删除的定时器名字：%s , 是否删除成功：%s", test_once_arg.name,err == ESP_OK ? "ok!\r\n" : "failed!\r\n");
 }
 
 /***********************************************/
@@ -78,11 +77,11 @@ void app_main(void)
     //开始创建一个周期定时器并且执行
     esp_err_t err = esp_timer_create(&test_period_arg,&test_p_handle);
     err = esp_timer_start_periodic(test_p_handle,1000*1000);
- //   ESP_LOGI(TAG,"重复周期运行定时器创建状态码：%s",err = ESP_OK ? "OK!\r\n" : "failed!\r\n");
+    ESP_LOGI(TAG,"重复周期运行定时器创建状态码：%s",err == ESP_OK ? "OK!\r\n" : "failed!\r\n");
     //开创一个单词周期定时器并且执行
 
     err = esp_timer_create(&test_once_arg,&test_o_handle);
     err = esp_timer_start_once(test_o_handle,10*1000*1000);
- //   ESP_LOGI(TAG,"单次运行定时器创建状态码：%s",err = ESP_OK ? "OK!\r\n" : "failed!\r\n");
+    ESP_LOGI(TAG,"单次运行定时器创建状态码：%s",err == ESP_OK ? "OK!\r\n" : "failed!\r\n");
    
 }
